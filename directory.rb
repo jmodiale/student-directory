86b27b5404c91e576525cdb34ab1df547b796f36
jmodiale <65014009+jmodiale@users.noreply.github.com>
2021-11-29 15:12:14 +0000

    array_and_loops

--- directory.rb
+++ directory.rb
@@ -1,18 +1,11 @@
-student_count = 11
+# let's put all students into an array
+student = ["The students of Villains Academy", "-------------", "Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched", "Michael Corleone", "Alex DeLarge", 
+"The Wicked Witch of the West", "Terminator", "Freddy Krueger", "The Joker", "Joffrey Baratheon", "Norman Bates"]
 
-#first we print the list of students
-puts "The students of Villains Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+student.each do |item|
+    puts item
+end
+
+student_count = student.length
 # finally, we print the total number of students
 puts "Overall, we have #{student_count} great students"
