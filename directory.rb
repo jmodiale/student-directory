dfb964c7052b1c82ad890174e3a29b95327d139b
jmodiale <65014009+jmodiale@users.noreply.github.com>
2021-11-29 15:53:38 +0000

    hashes

--- directory.rb
+++ directory.rb
@@ -1,6 +1,19 @@
 # let's put all students into an array
-students = ["Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched", "Michael Corleone", "Alex DeLarge", 
-"The Wicked Witch of the West", "Terminator", "Freddy Krueger", "The Joker", "Joffrey Baratheon", "Norman Bates"]
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
+
+puts students.inspect
 
 def print_header
   puts "The students of Villains Academy"
@@ -8,8 +21,8 @@ def print_header
 end
 
 def print_names(arr)
-  arr.each {|name|
-    puts name
+  arr.each {|student|
+   puts "#{student[:name]} is in #{student[:cohort]} cohort"
   }
 end
 
