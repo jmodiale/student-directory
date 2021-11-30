22ebd801dc5df8f44ee1594d2f9714ad91406f7d
jmodiale <65014009+jmodiale@users.noreply.github.com>
2021-11-30 10:18:39 +0000

    working method

--- new_directory.rb
+++ new_directory.rb
@@ -1,19 +1,31 @@
 # let's put all students into an array
 def input_students
   puts "Please enter a name:"
-  arr = []
+  students = []
   user_input = gets.chomp 
 
   while user_input.length > 0
-    arr.push(user_input)
+    students.push({name: user_input, cohort: :november})
+    puts "Now we have #{students.count} students"
     user_input = gets.chomp
   break if user_input.length < 0
   end
 
-  arr.each {|student|
-      puts "#{student} is in November cohort"
+  print_header
+  students.each {|student|
+      puts "#{student[:name]} is in #{student[:cohort].capitalize} cohort"
     }
+  # puts students.inspect
+  print_footer(students)
+end
 
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_footer(arr)
+  puts "-------------"
   puts "Overall, we have #{arr.count} great students"
 end
 
