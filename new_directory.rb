9bfc155d0a7837c84b4ed0b8ec753e807ed942c4
jmodiale <65014009+jmodiale@users.noreply.github.com>
2021-11-30 17:22:17 +0000

    update

--- new_directory.rb
+++ new_directory.rb
@@ -1,22 +1,27 @@
 # let's put all students into an array
+@students = [] # an empty array accessible to all methods
+
 def input_students
   puts "Please enter a name:"
-  students = []
   user_input = gets.chomp 
 
   while user_input.length > 0
-    students.push({name: user_input, cohort: :november})
-    puts "Now we have #{students.count} students"
+    @students.push({name: user_input, cohort: :november})
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
+    else
+      puts "Now we have #{@students.count} students"
+    end
     user_input = gets.chomp
   break if user_input.length < 0
   end
+  # puts students.inspect
+end
 
-  print_header
-  students.each {|student|
+def print_students_list
+  @students.each {|student|
       puts "#{student[:name]} is in #{student[:cohort].capitalize} cohort"
     }
-  # puts students.inspect
-  print_footer(students)
 end
 
 def print_header
@@ -24,9 +29,46 @@ def print_header
   puts "-------------"
 end
 
-def print_footer(arr)
+def print_footer
   puts "-------------"
-  puts "Overall, we have #{arr.count} great students"
+  puts "Overall, we have #{@students.count} great students"
 end
 
-input_students()
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+      when "1"
+        input_students
+      when "2"
+        show_students
+      when "9"
+        exit # this will cause the program to terminate
+      else
+        puts "I don't know what you meant, try again"
+    end
+end
+
+def interactive_menu
+  # 4. repeat from step 1
+  loop do
+  # 1. print the menu and ask the user what to do
+    print_menu 
+
+  # 2. read the input and save it into a variable && # 3. do what the user has asked
+    process(gets.chomp)
+  end
+end
+
+# input_students()
+interactive_menu
